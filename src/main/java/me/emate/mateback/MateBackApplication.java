c050e28e287260589f3d0c3183211c5d6caac775
Woon <81662836+uasaha@users.noreply.github.com>
2023-07-19 19:51:45 +0900

    Add spring-boot app
    Add spring-boot app

---
+++ src/main/java/me/emate/mateback/MateBackApplication.java
@@ -0,0 +1,13 @@
+package me.emate.mateback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/emate/mateback/MateBackApplicationTests.java
@@ -0,0 +1,13 @@
+package me.emate.mateback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
