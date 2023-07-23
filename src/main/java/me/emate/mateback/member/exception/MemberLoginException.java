b481a7e4004bb86be57c5f3b53d7cdd8334b9c07
운석 <istana000@gmail.com>
2023-07-23 20:02:36 +0900

    [Feat] Member 관련 예외 추가

--- src/main/java/me/emate/mateback/member/exception/MemberLoginException.java
+++ src/main/java/me/emate/mateback/member/exception/MemberLoginException.java
@@ -1,7 +1,7 @@
 package me.emate.mateback.member.exception;
 
 public class MemberLoginException extends RuntimeException {
-    private static String MESSAGE = "로그인 예외입니다.";
+    private static final String MESSAGE = "로그인 예외입니다.";
 
     public MemberLoginException() {
         super(MESSAGE);
